9e0323783e6881426a628d0ad4e5f1a11b406b40
Ремизова Ирина <w.warmbl@gmail.com>
2022-06-04 00:19:55 +0500

    Add touchmove (with bug)

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,7 @@ images.forEach((image, idx) => {
 })
 
 
-var doc = window.document,
+let doc = window.document,
     context = doc.querySelector('.js-slider'),
     items = doc.querySelectorAll('.js-item'),
     clones = [],
@@ -44,7 +44,7 @@ function reCalc () {
     clonesWidth = getClonesWidth()
   
     if (scrollPos <= 0) {
-      setScrollPos(1) // Прокрутка на 1 пиксель, чтобы разрешить прокрутку влево
+      setScrollPos(400) // Прокрутка на 1 пиксель, чтобы разрешить прокрутку влево
     }
 }
 
@@ -80,7 +80,7 @@ function onLoad () {
       context.appendChild(clone)
       clone.classList.add('js-clone')
       let what = doc.querySelectorAll('.js-clone')
-      console.log(what)
+      //console.log(what)
     })
 
     clones = context.querySelectorAll('.js-clone')
@@ -97,3 +97,81 @@ function onLoad () {
 }
 
 window.onload = onLoad
+
+
+
+
+////////////////////////////////////////////////////////////////////////
+// Draggable slider
+/////////////////////////////////////////////////////////////////////////
+
+let isDown = false;
+let startX;
+let scrollLeft;
+const slider = document.querySelector('.js-slider');
+
+// прекратить действие, когда клик отпущен
+const end = () => {
+	isDown = false;
+  slider.classList.remove('active');
+}
+
+// начать действие драгслайдера
+const start = (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX || e.touches[0].pageX - slider.offsetLeft;
+  
+  /*
+  console.log('первый параметр старта');
+  console.log(e.pageX);
+  //console.log(e.touches[0].pageX);
+  console.log('третий параметр старта');
+  console.log(slider.offsetLeft);
+  */
+
+  scrollLeft = slider.scrollLeft;	
+}
+
+//  помогити
+const move = (e) => {
+
+  // Пока мышка не опущена, ниче не делай
+	if(!isDown) return;
+
+  // Иначе делай это:
+  reCalc () // пересчет координат - позволяет скролить бесконечно
+  e.preventDefault(); // предотвращение события
+
+  const x = e.pageX || e.touches[0].pageX - slider.offsetLeft;
+  
+  /*
+  console.log('первый параметр мува');
+  console.log(e.pageX);
+  console.log('третий параметр мува');
+  console.log(slider.offsetLeft);
+  */
+
+  // x - координата (относительно слайдера) по горизонтали, когда отпустил клик
+  // startX - координата по горизонтали (относительно слайдера), когда нажал клик
+  const dist = (x - startX); // вычисляется при каждом сдвиге на один пиксель
+                             // берется координата клика
+                             // от этой координаты, если смещение влево, то отрицательные значения
+                             // если вмещение вправо, то положительные значения
+  console.log(dist);
+  slider.scrollLeft = scrollLeft - dist; // смещение в ту же сторону
+  
+  //scrollUpdate ()
+}
+
+(() => {
+	slider.addEventListener('mousedown', start);
+	slider.addEventListener('touchstart', start);
+
+	slider.addEventListener('mousemove', move); // работает при каждом сдвиге на пиксель
+	slider.addEventListener('touchmove', move);
+
+	slider.addEventListener('mouseleave', end);
+	slider.addEventListener('mouseup', end);
+	slider.addEventListener('touchend', end);
+})();
