ff54ac343dcc2a1ebfd9cecded9ec9bb7b50c969
Ремизова Ирина <w.warmbl@gmail.com>
2022-06-05 21:47:32 +0500

    Fix draggable slider

--- scripts/script.js
+++ scripts/script.js
@@ -5,9 +5,8 @@ images.forEach((image, idx) => {
   image.style.backgroundImage = `url(./images/${idx+1}.jpg)`
 })
 
-
 let doc = window.document,
-    context = doc.querySelector('.js-slider'),
+    slider = doc.querySelector('.js-slider'),
     items = doc.querySelectorAll('.js-item'),
     clones = [],
     disableScroll = false,
@@ -15,100 +14,67 @@ let doc = window.document,
     scrollPos = 0,
     clonesWidth = 0
 
-
 // ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ
 // Позиция по горизонтали
 function getScrollPos () {
-    return (context.pageXOffset || context.scrollLeft) - (context.clientLeft || 0)
+  return (slider.pageXOffset || slider.scrollLeft) - (slider.clientLeft || 0)
 }
 
 function setScrollPos (pos) {
-    context.scrollLeft = pos
+  slider.scrollLeft = pos
 }
-
 
 // Ширина копий элементов
 function getClonesWidth () {
-    clonesWidth = 0
-    Array.from(clones, clone => {
-      clonesWidth = clonesWidth + clone.offsetWidth
-    })
-    return clonesWidth
+  clonesWidth = 0
+  Array.from(clones, clone => {
+    clonesWidth = clonesWidth + clone.offsetWidth
+  })
+  return clonesWidth
 }
 
 // ОСНОВНЫЕ ФУНКЦИИ
 // Пересчет
 function reCalc () {
-    scrollPos = getScrollPos()
-    scrollWidth = context.scrollWidth
-    clonesWidth = getClonesWidth()
+  scrollPos = getScrollPos()
+  scrollWidth = slider.scrollWidth
+  clonesWidth = getClonesWidth()
   
-    if (scrollPos <= 0) {
-      setScrollPos(400) // Прокрутка на 1 пиксель, чтобы разрешить прокрутку влево
-    }
+  if (scrollPos <= 0) {
+    setScrollPos(1)
+  }
 }
 
 // Бесконечная прокрутка
-// ИСПРАВИТЬ БАГ С 400 ПИКСЕЛЯМИ  setScrollPos
 function scrollUpdate () {
-    if (!disableScroll) {
-      scrollPos = getScrollPos()
+  if (!disableScroll) {
+    scrollPos = getScrollPos()
   
-      if (clonesWidth + scrollPos >= scrollWidth) {
-        // Прокрутка влево, когда доберется до правого края
-        setScrollPos(400) //Прокрутка вниз на 400 пикселей ДОРАБОТАТЬ - НУЖНО 1 ПИКСЕЛЬ
-        disableScroll = true
-      } else if (scrollPos <= 0) {
-        // Прокрутка вправо, когда доберется до левого края
-        setScrollPos(scrollWidth - clonesWidth - 400)
-        disableScroll = true
-      }
+    if (clonesWidth + scrollPos >= scrollWidth) {
+      // Прокрутка влево, когда доберется до правого края
+      setScrollPos(1)
+      disableScroll = true
+    } else if (scrollPos <= 0) {
+      // Прокрутка вправо, когда доберется до левого края
+      setScrollPos(scrollWidth - clonesWidth)
+      disableScroll = true
     }
+  }
 
-    if (disableScroll) {
-        window.setTimeout(function () {
-          disableScroll = false
-        }, 40)
-      }
+  if (disableScroll) {
+    window.setTimeout(function () {
+      disableScroll = false
+    }, 40)
+  }
 }
-
-// Выполняется при загрузке страницы
-function onLoad () {
-
-    Array.from(items, item => {
-      const clone = item.cloneNode(true)
-      context.appendChild(clone)
-      clone.classList.add('js-clone')
-      let what = doc.querySelectorAll('.js-clone')
-      //console.log(what)
-    })
-
-    clones = context.querySelectorAll('.js-clone')
-  
-    reCalc()
-  
-    context.addEventListener('scroll', function () {
-      window.requestAnimationFrame(scrollUpdate)
-    }, false)
-  
-    window.addEventListener('resize', function () {
-      window.requestAnimationFrame(reCalc)
-    }, false)
-}
-
-window.onload = onLoad
-
-
-
 
 ////////////////////////////////////////////////////////////////////////
 // Draggable slider
-/////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
 
 let isDown = false;
 let startX;
 let scrollLeft;
-const slider = document.querySelector('.js-slider');
 
 // прекратить действие, когда клик отпущен
 const end = () => {
@@ -116,62 +82,66 @@ const end = () => {
   slider.classList.remove('active');
 }
 
-// начать действие драгслайдера
+// начать действие
 const start = (e) => {
   isDown = true;
   slider.classList.add('active');
   startX = e.pageX || e.touches[0].pageX - slider.offsetLeft;
-  
-  /*
-  console.log('первый параметр старта');
-  console.log(e.pageX);
-  //console.log(e.touches[0].pageX);
-  console.log('третий параметр старта');
-  console.log(slider.offsetLeft);
-  */
-
   scrollLeft = slider.scrollLeft;	
 }
 
-//  помогити
 const move = (e) => {
-
-  // Пока мышка не опущена, ниче не делай
+  scrollPos = getScrollPos()
 	if(!isDown) return;
-
-  // Иначе делай это:
-  reCalc () // пересчет координат - позволяет скролить бесконечно
   e.preventDefault(); // предотвращение события
-
   const x = e.pageX || e.touches[0].pageX - slider.offsetLeft;
   
-  /*
-  console.log('первый параметр мува');
-  console.log(e.pageX);
-  console.log('третий параметр мува');
-  console.log(slider.offsetLeft);
-  */
+  const dist = (x - startX); 
+  slider.scrollLeft = scrollLeft - dist;
 
-  // x - координата (относительно слайдера) по горизонтали, когда отпустил клик
-  // startX - координата по горизонтали (относительно слайдера), когда нажал клик
-  const dist = (x - startX); // вычисляется при каждом сдвиге на один пиксель
-                             // берется координата клика
-                             // от этой координаты, если смещение влево, то отрицательные значения
-                             // если вмещение вправо, то положительные значения
-  console.log(dist);
-  slider.scrollLeft = scrollLeft - dist; // смещение в ту же сторону
-  
-  //scrollUpdate ()
+  if(scrollPos == -1 || scrollPos == 0){
+    console.log(scrollPos + ' Нулевое значение - Левая граница')
+
+    setScrollPos(scrollWidth - clonesWidth)
+    scrollLeft = slider.scrollLeft + dist;
+  }
+  else if (clonesWidth + scrollPos >= scrollWidth){
+    console.log(scrollPos + ' Больше нуля - Правая граница') 
+    setScrollPos(1)
+    scrollLeft = slider.scrollLeft + dist;
+  }
 }
 
-(() => {
-	slider.addEventListener('mousedown', start);
+// Выполняется при загрузке страницы
+function onLoad () {
+
+  Array.from(items, item => {
+    const clone = item.cloneNode(true)
+    slider.appendChild(clone)
+    clone.classList.add('js-clone')
+  })
+
+  clones = slider.querySelectorAll('.js-clone')
+  
+  reCalc()
+  
+  slider.addEventListener('scroll', function () {
+    window.requestAnimationFrame(scrollUpdate)
+  }, false)
+
+  window.addEventListener('resize', function () {
+    window.requestAnimationFrame(reCalc)
+  }, false)
+
+  slider.addEventListener('mousedown', start);
 	slider.addEventListener('touchstart', start);
 
-	slider.addEventListener('mousemove', move); // работает при каждом сдвиге на пиксель
+	slider.addEventListener('mousemove', move); // работает при каждом сдвиге курсора на пиксель
 	slider.addEventListener('touchmove', move);
 
 	slider.addEventListener('mouseleave', end);
 	slider.addEventListener('mouseup', end);
 	slider.addEventListener('touchend', end);
-})();
+}
+
+window.onload = onLoad
